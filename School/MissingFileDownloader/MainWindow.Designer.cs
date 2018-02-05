e084378e783f6eeebc15ad26bbcc93ee2bc98d83
RsWorkTechnologies <renpengfei_8888@163.com>
2018-02-05 12:36:00 +0800

    temp save

---
+++ School/MissingFileDownloader/MainWindow.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace MissingFileDownloader
+{
+	partial class MainWindow
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.modernButton1 = new RsWork.UI.Controls.ModernButton();
+			this.modernButton2 = new RsWork.UI.Controls.ModernButton();
+			this.modernButton3 = new RsWork.UI.Controls.ModernButton();
+			this.modernButton4 = new RsWork.UI.Controls.ModernButton();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Titles = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Introductions = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Else = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// title
+			// 
+			this.title.Size = new System.Drawing.Size(534, 46);
+			this.title.Text = "BiSS 云学习公共平台下载管理器";
+			// 
+			// Context
+			// 
+			this.Context.Size = new System.Drawing.Size(308, 21);
+			this.Context.Text = "Repo 地址:https://kxxt.github.io/School";
+			// 
+			// modernButton1
+			// 
+			this.modernButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.modernButton1.BackColor = System.Drawing.Color.Transparent;
+			this.modernButton1.BorderColor = System.Drawing.Color.White;
+			this.modernButton1.DisabledBorderColor = System.Drawing.Color.DarkGray;
+			this.modernButton1.DisabledForeColor = System.Drawing.Color.DarkGray;
+			this.modernButton1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.modernButton1.FlatAppearance.BorderSize = 2;
+			this.modernButton1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+			this.modernButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+			this.modernButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.modernButton1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.modernButton1.ForeColor = System.Drawing.Color.White;
+			this.modernButton1.Location = new System.Drawing.Point(1041, 8);
+			this.modernButton1.Name = "modernButton1";
+			this.modernButton1.Size = new System.Drawing.Size(32, 32);
+			this.modernButton1.TabIndex = 2;
+			this.modernButton1.Text = "X";
+			this.modernButton1.UseVisualStyleBackColor = false;
+			this.modernButton1.Click += new System.EventHandler(this.modernButton1_Click);
+			// 
+			// modernButton2
+			// 
+			this.modernButton2.BackColor = System.Drawing.Color.Transparent;
+			this.modernButton2.BorderColor = System.Drawing.Color.White;
+			this.modernButton2.DisabledBorderColor = System.Drawing.Color.DarkGray;
+			this.modernButton2.DisabledForeColor = System.Drawing.Color.DarkGray;
+			this.modernButton2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.modernButton2.FlatAppearance.BorderSize = 2;
+			this.modernButton2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.modernButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.modernButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.modernButton2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.modernButton2.ForeColor = System.Drawing.Color.White;
+			this.modernButton2.Location = new System.Drawing.Point(17, 84);
+			this.modernButton2.Name = "modernButton2";
+			this.modernButton2.Size = new System.Drawing.Size(166, 38);
+			this.modernButton2.TabIndex = 3;
+			this.modernButton2.Text = "打开项目GitHub(&G)";
+			this.modernButton2.UseVisualStyleBackColor = false;
+			// 
+			// modernButton3
+			// 
+			this.modernButton3.BackColor = System.Drawing.Color.Transparent;
+			this.modernButton3.BorderColor = System.Drawing.Color.White;
+			this.modernButton3.DisabledBorderColor = System.Drawing.Color.DarkGray;
+			this.modernButton3.DisabledForeColor = System.Drawing.Color.DarkGray;
+			this.modernButton3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.modernButton3.FlatAppearance.BorderSize = 2;
+			this.modernButton3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.modernButton3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.modernButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.modernButton3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.modernButton3.ForeColor = System.Drawing.Color.White;
+			this.modernButton3.Location = new System.Drawing.Point(361, 84);
+			this.modernButton3.Name = "modernButton3";
+			this.modernButton3.Size = new System.Drawing.Size(166, 38);
+			this.modernButton3.TabIndex = 4;
+			this.modernButton3.Text = "联系作者(&C)";
+			this.modernButton3.UseVisualStyleBackColor = false;
+			// 
+			// modernButton4
+			// 
+			this.modernButton4.BackColor = System.Drawing.Color.Transparent;
+			this.modernButton4.BorderColor = System.Drawing.Color.White;
+			this.modernButton4.DisabledBorderColor = System.Drawing.Color.DarkGray;
+			this.modernButton4.DisabledForeColor = System.Drawing.Color.DarkGray;
+			this.modernButton4.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.modernButton4.FlatAppearance.BorderSize = 2;
+			this.modernButton4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.modernButton4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.modernButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.modernButton4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.modernButton4.ForeColor = System.Drawing.Color.White;
+			this.modernButton4.Location = new System.Drawing.Point(189, 84);
+			this.modernButton4.Name = "modernButton4";
+			this.modernButton4.Size = new System.Drawing.Size(166, 38);
+			this.modernButton4.TabIndex = 5;
+			this.modernButton4.Text = "拷贝整个项目(&W)";
+			this.modernButton4.UseVisualStyleBackColor = false;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.BackgroundColor = System.Drawing.Color.DeepSkyBlue;
+			this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Titles,
+            this.Introductions,
+            this.Else});
+			this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+			this.dataGridView1.GridColor = System.Drawing.Color.LightGray;
+			this.dataGridView1.Location = new System.Drawing.Point(17, 128);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowTemplate.Height = 23;
+			this.dataGridView1.ShowEditingIcon = false;
+			this.dataGridView1.Size = new System.Drawing.Size(544, 486);
+			this.dataGridView1.TabIndex = 6;
+			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+			// 
+			// Titles
+			// 
+			this.Titles.HeaderText = "标题";
+			this.Titles.Name = "Titles";
+			this.Titles.ReadOnly = true;
+			// 
+			// Introductions
+			// 
+			this.Introductions.HeaderText = "简介";
+			this.Introductions.Name = "Introductions";
+			this.Introductions.ReadOnly = true;
+			// 
+			// Else
+			// 
+			this.Else.HeaderText = "备注";
+			this.Else.Name = "Else";
+			this.Else.ReadOnly = true;
+			// 
+			// MainWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 26F);
+			this.ClientSize = new System.Drawing.Size(1085, 628);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.modernButton4);
+			this.Controls.Add(this.modernButton3);
+			this.Controls.Add(this.modernButton2);
+			this.Controls.Add(this.modernButton1);
+			this.MaximizeBox = false;
+			this.Name = "MainWindow";
+			this.ShowIcon = true;
+			this.ShowInTaskbar = true;
+			this.Controls.SetChildIndex(this.title, 0);
+			this.Controls.SetChildIndex(this.Context, 0);
+			this.Controls.SetChildIndex(this.modernButton1, 0);
+			this.Controls.SetChildIndex(this.modernButton2, 0);
+			this.Controls.SetChildIndex(this.modernButton3, 0);
+			this.Controls.SetChildIndex(this.modernButton4, 0);
+			this.Controls.SetChildIndex(this.dataGridView1, 0);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private RsWork.UI.Controls.ModernButton modernButton1;
+		private RsWork.UI.Controls.ModernButton modernButton2;
+		private RsWork.UI.Controls.ModernButton modernButton3;
+		private RsWork.UI.Controls.ModernButton modernButton4;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Titles;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Introductions;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Else;
+	}
+}
---
+++ School/MissingFileDownloader/MainWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MissingFileDownloader
+{
+	public partial class MainWindow : RsWork.UI.Windows.ModernDialogBase
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void modernButton1_Click(object sender, EventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+	}
+}
